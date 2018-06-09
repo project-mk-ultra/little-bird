632ad6eaee8ec84b8446f1ab8cbccb189feee2a6
ColaLight77 <kocher.julian@gmx.at>
2018-06-09 08:21:14 +0200

    added differing bit function

--- dht/utils.py
+++ dht/utils.py
@@ -1,2 +1,9 @@
 class Utils:
-    pass
+    @staticmethod
+    def largest_differing_bit(value1, value2):
+        distance = value1 ^ value2
+        length = -1
+        while distance:
+            distance >>= 1
+            length += 1
+        return max(0, length)
